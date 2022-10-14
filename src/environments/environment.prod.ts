89c035066f269356d0ff0105f978fcc73ac0d01f
ayushk26 <ayushk681@gmail.com>
2022-10-14 15:55:09 +0530

    Add server url in prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  backendURL:"http://10.105.177.14",
-  redirectURL:"http://10.105.177.14/dashboard/login",
+  backendURL:"https://instamess.gymkhana.iitb.ac.in",
+  redirectURL:"https://instamess.gymkhana.iitb.ac.in/dashboard/login",
   clientID:"1dE3mAQY7EvCjw5IdHgoNtbutjgZy14CyIsWKRK8"
 };
