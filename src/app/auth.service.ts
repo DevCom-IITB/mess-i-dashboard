81ffb20d7aa8efe0e28aa24e7ac7c78e0bcc2056
git stash <git@stash>
2023-05-05 00:22:08 +0530

    index on main: 05b08cc fixed dashboard redirect to home

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -10,15 +10,19 @@ import { environment } from './../environments/environment';
 export class AuthService {
   logged_in = false;
   token:any;
-  is_admin:String;
-  roll_no:string;
+  is_admin:boolean;
+  is_staff:boolean;
+  is_rebate:boolean;
+  is_student:boolean;
+  roll_no:any;
   url = environment.backendURL+'/api/dash/auth';
   constructor(private http:HttpClient, private router:Router) { 
     this.token = sessionStorage.getItem("mess-i-token");
-    this.is_admin = sessionStorage.getItem("mess-i-admin")??"";
-    this.is_admin = "staff"
-    this.token = "8e020f89-6083-4a2f-8aec-d4e42d457dde";
-    this.roll_no = "";
+    this.roll_no = sessionStorage.getItem("mess-i-roll");
+    this.is_admin = JSON.parse(sessionStorage.getItem("mess-i-admin") ?? "false");
+    this.is_staff = JSON.parse(sessionStorage.getItem("mess-i-staff") ?? "false");
+    this.is_rebate = JSON.parse(sessionStorage.getItem("mess-i-rebate") ?? "false");
+    this.is_student = JSON.parse(sessionStorage.getItem("mess-i-student") ?? "false");
 
     if(this.token!=null){
       this.logged_in = true;
@@ -35,11 +39,26 @@ export class AuthService {
     return this.http.get(this.url,header_node).subscribe((res:any) => {
       this.token = res.token;
       this.logged_in = true;
-      this.is_admin = res.access_level;
+      
+      this.is_admin = res.is_admin;
+      this.is_rebate = res.is_rebate;
+      this.is_staff = res.is_staff;
+      this.is_student = res.is_student;
+
       this.roll_no = res.roll;
       sessionStorage.setItem("mess-i-token",res.token);
-      sessionStorage.setItem("mess-i-admin",res.is_admin);
-      this.router.navigate(['overview']);
+      sessionStorage.setItem("mess-i-roll",res.roll);
+      
+      sessionStorage.setItem("mess-i-admin",res.is_admin.toString());
+      sessionStorage.setItem("mess-i-staff",res.is_staff.toString());
+      sessionStorage.setItem("mess-i-rebate",res.is_rebate.toString());
+      sessionStorage.setItem("mess-i-student",res.is_student.toString());
+
+      if(this.isStaff()){
+        this.router.navigate(['home']);
+      }else{
+        this.router.navigate(['rebate']);
+      }
     })
   }
   logoutUser(){
@@ -49,10 +68,23 @@ export class AuthService {
   isLoggedIn(){
     return this.logged_in;
   }
+  
   isAdmin(){
-    if(this.is_admin==="staff") return true;
-    else return false;
+    return this.is_admin;
   }
+  
+  isStaff(){
+    return (this.is_admin || this.is_rebate || this.is_staff);
+  }
+
+  isStudent(){
+    return this.is_student;
+  }
+  
+  isRebate(){
+    return (this.is_admin || this.is_rebate);
+  }
+
   getToken(){
     return this.token;
   }
