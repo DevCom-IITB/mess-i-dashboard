65cfeb26f70fd72b08bd97f6f3147f507de0e5f4
mahesh_kumar <mahesh.99prajapat@gmail.com>
2024-01-04 13:43:28 +0530

    Revert "Revert "Refine stats page, update endpoints being used""
    This reverts commit 2ac181720a152276c5efe3d788f6ed722c57872c.

--- src/app/statistics/statistics.component.ts
+++ src/app/statistics/statistics.component.ts
@@ -10,7 +10,8 @@ import { StudentdataService } from '../studentdata.service';
   styleUrls: ['./statistics.component.css']
 })
 export class StatisticsComponent implements OnInit {
-
+  hostel_selectable: boolean = false;
+  roll_selectable: boolean = false;
   allowedHostels:boolean[] = new Array<boolean>(22);
   hostelmessHistory:any;
   studentmessHistory:any;
@@ -26,7 +27,8 @@ export class StatisticsComponent implements OnInit {
     }
   }
   ngOnInit(): void {
-    this.getAdminHostel()
+    this.getAdminHostel();
+    this.hostel_selectable = this.roll_selectable = this.auth.isAdmin();
   }
 
   getAdminHostel(){
@@ -44,7 +46,8 @@ export class StatisticsComponent implements OnInit {
       console.log(res)
     })
   }
-  cleanData(history:any){
+  orgHostelData(history:any){
+    if(this.noOfDays.length == 0) return [];
     let mp = new Map<string,number[]>([
       ['Breakfast',[]],['Lunch',[]],['Snacks',[]],['Dinner',[]],['Milk',[]],['Egg',[]],
     ])
@@ -68,28 +71,32 @@ export class StatisticsComponent implements OnInit {
     
   }
 
-  async getMonthMessData(data: any){
-    
+  async plotData(data: any){
+    this.plotHostelData(data);
+    this.plotStudentData(data);
+  }
+  async plotHostelData(data: any){
     if (data.form.value.year&&data.form.value.month) {
       let num =  new Date(parseInt(data.form.value.year), parseInt(data.form.value.month), 0).getDate();
       this.noOfDays = Array(num).fill(1).map((x,i) => (i + 1).toString());
       this.noOfDays2 = Array(num).fill(`${data.form.value.year}-${data.form.value.month}-${1}`).map((x,i) => (`${data.form.value.year}-${data.form.value.month}-${i+1}`));
-      this.service.getMonthlyMessdata(data.form.value.hostel,data.form.value.year,data.form.value.month).then((res)=>
+      this.service.getHostelStats(data.form.value.hostel,data.form.value.year,data.form.value.month).then((res)=>
       {
         let history = res;
-        this.hostelmessHistory = this.cleanData(history);
+        this.hostelmessHistory = this.orgHostelData(history);
         this.plot.plotMonthlyMess("Mess Stats","mess_data",this.noOfDays2,this.hostelmessHistory.get('Breakfast')!,this.hostelmessHistory.get('Lunch')!,this.hostelmessHistory.get('Snacks')!,this.hostelmessHistory.get('Dinner')!,this.hostelmessHistory.get('Milk')!,this.hostelmessHistory.get('Egg')!);
       }).catch((res)=>{
         console.log(res)
-        this.hostelmessHistory = this.cleanData({})
+        this.hostelmessHistory = this.orgHostelData({})
       });
     }
   }
 
-  student_cleanData(history:any,noOfDays:string[]){
+  orgStudentData(history:any,noOfDays:string[]){
     let mp = new Map<string,number[]>([
       ['Breakfast',[]],['Lunch',[]],['Snacks',[]],['Dinner',[]],['Milk',[]],['Egg',[]],
     ])
+    if(noOfDays.length == 0) return [];
     for(let j=0;j<noOfDays.length;j++){
       if(!(noOfDays[j]in history)){
         for(let key of mp.keys()) {
@@ -110,22 +117,21 @@ export class StatisticsComponent implements OnInit {
     
   }
 
-  async getMonthStudentData(data: any){    
+  async plotStudentData(data: any){    
     if (data.form.value.year&&data.form.value.month) {
       let num =  new Date(parseInt(data.form.value.year), parseInt(data.form.value.month), 0).getDate();
       let noOfDays = Array(num).fill(1).map((x,i) => (i + 1).toString());
-      // console.log(this.service.getMonthlytotaldata(this.auth.roll_no,data.form.value.year,data.form.value.month))
-      this.service.getMonthlydata(this.auth.roll_no,data.form.value.year,data.form.value.month).then((res)=>
+      this.service.getStudentStats(this.roll_selectable ? data.form.value.roll : this.auth.roll_no,data.form.value.year,data.form.value.month).then((res)=>
       {
         let history = res;
-        this.studentmessHistory = this.student_cleanData(history, noOfDays);
+        this.studentmessHistory = this.orgStudentData(history, noOfDays);
         let data: number[][] = Array.from(this.studentmessHistory.values() as Iterable<number[]>).map((value) => [...value]);
         let sums: number[] = data.map((subarray) => subarray.reduce((acc, value) => acc + value, 0));
         this.plot.plotStudentHeatmapData("Student Stats","student_data",data,noOfDays);
         this.plot.plotStudentPieData("pie", sums)
       }).catch((res)=>{
         console.log(res)
-        this.studentmessHistory = this.student_cleanData({},[])
+        this.studentmessHistory = this.orgStudentData({},[])
       });
     }
   }
