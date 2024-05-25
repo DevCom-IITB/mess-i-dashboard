cc38ac03d91285122698379ea48f561239b8d14c
Hari Shankar Karthik <harishankarkarthik.93@gmail.com>
2024-05-25 16:25:42 +0530

    ADDED: API Call for messmanagerlogin

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -16,6 +16,7 @@ export class AuthService {
   is_student:boolean;
   roll_no:any;
   url = environment.backendURL+'/api/dash/auth';
+  urlMessManager = environment.backendURL+'/api/dash/auth/messmanager';
   constructor(private http:HttpClient, private router:Router) { 
     this.token = sessionStorage.getItem("mess-i-token");
     this.roll_no = sessionStorage.getItem("mess-i-roll");
@@ -57,6 +58,38 @@ export class AuthService {
       this.router.navigate(['landing']);
     })
   }
+
+  loginMessManager(username: string, password: string) {
+    let params = new HttpParams();
+
+    params = params.append('username', username);
+    params = params.append('password', password);
+    let header_node = {
+      headers: new HttpHeaders({ 'rejectUnauthorized': 'false' }),
+      params,
+    };
+    return this.http.get(this.urlMessManager, header_node).subscribe((res: any) => {
+      this.token = res.token;
+      this.logged_in = true;
+      
+      this.is_admin = res.is_admin;
+      this.is_rebate = res.is_rebate;
+      this.is_staff = res.is_staff;
+      this.is_student = res.is_student;
+
+      this.roll_no = res.roll;
+      sessionStorage.setItem("mess-i-token",res.token);
+      sessionStorage.setItem("mess-i-roll",res.roll);
+      
+      sessionStorage.setItem("mess-i-admin",res.is_admin.toString());
+      sessionStorage.setItem("mess-i-staff",res.is_staff.toString());
+      sessionStorage.setItem("mess-i-rebate",res.is_rebate.toString());
+      sessionStorage.setItem("mess-i-student",res.is_student.toString());
+
+      this.router.navigate(['landing']);
+    });
+  }
+  
   logoutUser(){
     this.token="";
     this.logged_in=false; 
