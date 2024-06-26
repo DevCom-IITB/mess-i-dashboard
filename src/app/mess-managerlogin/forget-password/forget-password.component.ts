52f4af6ab053c003d3d40c87b9c3a56c19cbfeed
Harsh <harshchoprajpr@gmail.com>
2024-06-26 13:12:07 +0530

    Added redirect for login page on forget password page and give alert for successful password change or error

--- src/app/mess-managerlogin/forget-password/forget-password.component.ts
+++ src/app/mess-managerlogin/forget-password/forget-password.component.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-forget-password',
@@ -12,7 +13,7 @@ export class ForgetPasswordComponent implements OnInit {
   public getJsonValue: any;
   public postJsonValue: any;
   public putJsonValue: any;
-  constructor(private http: HttpClient, private fb: FormBuilder, private authService: AuthService) { }
+  constructor(private http: HttpClient, private fb: FormBuilder, private authService: AuthService , private router : Router) { }
 
   forgetForm: FormGroup;
   error: any = null;
@@ -29,10 +30,27 @@ export class ForgetPasswordComponent implements OnInit {
         (res) => {
           console.log(res);
           this.success = true;
+          alert('New password sent to registerd email successfully!');
+          this.router.navigate(['/mess-manager']);
         },
         (err) => {
           console.log(err);
           this.error = err;
+          let errorMessage = 'An error occurred while sending the password reset email. Please try again later.';
+          if (err.status === 400) {
+            errorMessage = 'Invalid email address. Please check the email address and try again.';
+          } else if (err.status === 404) {
+            errorMessage = 'Email address not found. Please check the email address and try again.';
+          } else if (err.status === 500) {
+            errorMessage = 'Server error. Please try again later.';
+          } else if (err.status === 0) {
+            errorMessage = 'Network error. Please check your internet connection and try again.';
+          }
+          alert(errorMessage);
+          const currentUrl = this.router.url;
+          this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+            this.router.navigate([currentUrl]);
+          });;
         }
       );
     // if (this.forgetForm.valid) { 
