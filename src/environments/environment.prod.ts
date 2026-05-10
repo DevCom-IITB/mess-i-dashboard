0408c0bf530a9d424b088dcb76f85fb97819ee5a
raj <rajmall.0206@gmail.com>
2026-05-11 02:00:16 +0530

    Added new env var to prod env file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  backendURL:"https://instamess.gymkhana.iitb.ac.in",
-  redirectURL:"https://instamess.gymkhana.iitb.ac.in/dashboard/login",
-  clientID:"1dE3mAQY7EvCjw5IdHgoNtbutjgZy14CyIsWKRK8"
+  llmTesting: false,
+  backendURL: "https://instamess.gymkhana.iitb.ac.in",
+  redirectURL: "https://instamess.gymkhana.iitb.ac.in/dashboard/login",
+  clientID: "1dE3mAQY7EvCjw5IdHgoNtbutjgZy14CyIsWKRK8",
 };
